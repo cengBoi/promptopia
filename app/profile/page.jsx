b9dbc10bfc5f3d778c988c1cfaf4ea470d25adfb
cengBoi <akcay.yigitc@gmail.com>
2023-07-11 16:52:12 +0300

    Editing posts are avaliable now in "my profile" page.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,6 +9,7 @@ const MyProfile = () => {
 
     const { data: session } = useSession();
     const [posts, setPosts] = useState([]);
+    const router = useRouter();
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -20,11 +21,11 @@ const MyProfile = () => {
         if (session?.user.id) fetchPosts();
     }, []);
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     };
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
 
     };
 
